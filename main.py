f14bc6cc8a445143ccf8be1508d2716b28de45cf
mikalpeterson <mikalpeterson99@gmail.com>
2022-07-20 15:26:41 -0600

    v0.3 BWA portion up and running

--- main.py
+++ main.py
@@ -67,62 +67,106 @@ def trimmomatics(path_to_trim, thread_num, in_fasta_f, in_fasta_r, out_prefix, o
 	out_pair2 = out_prefix + '_reverse_paired.fq.gz'
 	out_unpair2 = out_prefix + '_reverse_unpaired.fq.gz'
 
+	# TODO keep/remove os.path stuff
+	out_pair1 = os.path.join(out_loc, out_pair1)
+	out_unpair1 = os.path.join(out_loc, out_unpair1)
+	out_pair2 = os.path.join(out_loc, out_pair2)
+	out_unpair2 = os.path.join(out_loc, out_unpair2)
+
 	#TODO Make this cleaner
-	trimcommand = 'java -jar '
-	trimcommand += path_to_trim
-	#trimcommand += ' org.usadellab.trimmomatic.TrimmomaticPE'
-	trimcommand += ' PE -threads '
-	trimcommand += str(thread_num)
-	trimcommand += ' -phred33 '
-	trimcommand += in_fasta_f
-	trimcommand += ' '
-	trimcommand += in_fasta_r
-	trimcommand += ' '
-	trimcommand += os.path.join(out_loc, out_pair1)
-	trimcommand += ' '
-	trimcommand += os.path.join(out_loc, out_unpair1)
-	trimcommand += ' '
-	trimcommand += os.path.join(out_loc, out_pair2)
-	trimcommand += ' '
-	trimcommand += os.path.join(out_loc, out_unpair2)
-	trimcommand += ' ILLUMINACLIP:'
-	trimcommand += adaptor_path
-	trimcommand += ':'
-	trimcommand += str(seed_mis)
-	trimcommand += ':'
-	trimcommand += str(pal_clip)
-	trimcommand += ':'
-	trimcommand += str(sim_clip)
-	trimcommand += ':'
-	trimcommand += str(min_adapt_len)
-	trimcommand += ':'
-	trimcommand += str(both_reads)
-	trimcommand += ' LEADING:'
-	trimcommand += str(leadq)
-	trimcommand += ' TRAILING:'
-	trimcommand += str(trailq)
-	trimcommand += ' MINLEN:'
-	trimcommand += str(min_len)
+	#TODO remove/keep os.path stuff
+	trim_command = 'java -jar '
+	trim_command += path_to_trim
+	#trim_command += ' org.usadellab.trimmomatic.TrimmomaticPE'
+	trim_command += ' PE -threads '
+	trim_command += str(thread_num)
+	trim_command += ' -phred33 '
+	trim_command += in_fasta_f
+	trim_command += ' '
+	trim_command += in_fasta_r
+	trim_command += ' '
+	#trim_command += os.path.join(out_loc, out_pair1)
+	trim_command += out_pair1
+	trim_command += ' '
+	#trim_command += os.path.join(out_loc, out_unpair1)
+	trim_command += out_unpair1
+	trim_command += ' '
+	#trim_command += os.path.join(out_loc, out_pair2)
+	trim_command += out_pair2
+	trim_command += ' '
+	#trim_command += os.path.join(out_loc, out_unpair2)
+	trim_command += out_unpair2
+	trim_command += ' ILLUMINACLIP:'
+	trim_command += adaptor_path
+	trim_command += ':'
+	trim_command += str(seed_mis)
+	trim_command += ':'
+	trim_command += str(pal_clip)
+	trim_command += ':'
+	trim_command += str(sim_clip)
+	trim_command += ':'
+	trim_command += str(min_adapt_len)
+	trim_command += ':'
+	trim_command += str(both_reads)
+	trim_command += ' LEADING:'
+	trim_command += str(leadq)
+	trim_command += ' TRAILING:'
+	trim_command += str(trailq)
+	trim_command += ' MINLEN:'
+	trim_command += str(min_len)
 
-	print(trimcommand)
+	print(trim_command)
 
-	# TODO Ensure proper forward and reverse files are being used
 	# Run trimmomatic
-	#os.system(trimcommand)
-	subprocess.Popen(trimcommand, shell=True).wait()
+	subprocess.Popen(trim_command, shell=True).wait()
 	print('output got')
+
 	# Return the output files
 	return out_pair1, out_unpair1 , out_pair2, out_unpair2
 
 
-def run_bwa(thread_num, trimmed_for_inputs, trimmed_rev_inputs, ref_gen, out_prefix):
-	#TODO Finish this
-	subprocess.run('bwa', 'index', ref_gen, out_prefix)
-	# TODO Figure out trimmed inputs
-	subprocess.run('bwa', 'mem', '-a', '-C', '-H', '-M', '-P', '-t', thread_num, out_prefix, trimmed_for_inputs, trimmed_rev_inputs)
-	# TODO Figure out parameters
+def run_bwa_index(ref_gen):
+	gen_index_input = 'bwa index ' + ref_gen
+	print(gen_index_input)
+	subprocess.Popen(gen_index_input, shell=True).wait()
 
-	# NOTE Used all parameters given, I suspect only -a, -M, -P is needed
+def run_bwa_mem(thread_num, trimmed_for_inputs, trimmed_rev_inputs, out_prefix, out_loc, ref_gen):
+	bwa_command = 'bwa mem -a -C -H -M -P -t '
+	bwa_command += str(thread_num)
+	bwa_command += ' '
+	# bwa_command += ' -k '
+	# bwa_command += min_seed_len
+	# bwa_command += '-w'
+	# bwa_command += band_width
+	# bwa_command += '-d'
+	# bwa_command += z_dropoff
+	# bwa_command += '-r'
+	# bwa_command += seed_split_ratio
+	# bwa_command += '-c'
+	# bwa_command += maxOcc
+	# bwa_command += '-A'
+	# bwa_command += matchScore
+	# bwa_command += '-B'
+	# bwa_command += mmPenalty
+	# bwa_command += '-O'
+	# bwa_command += gapOpenPen
+	# bwa_command += '-E'
+	# bwa_command += gapExtPen
+	# bwa_command += '-L'
+	# bwa_command += clipPen
+	# bwa_command += '-U'
+	# bwa_command += unpairPen
+	# bwa_command += '-R'
+	# bwa_command += RGline
+	# bwa_command += '-v'
+	# bwa_command += verboseLevel
+	# TODO add in db.prefix (genome)
+	bwa_command += ref_gen + ' '
+	bwa_command += trimmed_for_inputs + ' '
+	bwa_command += trimmed_rev_inputs + ' > '
+	bwa_command += out_loc + '/' + out_prefix + '_aln-se.sam'
+	print(bwa_command)
+	subprocess.Popen(bwa_command, shell=True).wait()
 
 
 def run_tiger():
@@ -132,14 +176,17 @@ def run_tiger():
 
 if __name__ == '__main__':
 	print("readtoTiger has started")
+
 	arguments = read_args()
+
 	output_dir_loc = create_output_directory(location=arguments.out_loc, prefix=arguments.out_prefix)
+
 	out_pair1, out_unpair1, out_pair2, out_unpair2 = trimmomatics(path_to_trim=arguments.trim_path,
 																	thread_num=arguments.thread_n[0],
 																	in_fasta_f=arguments.in_fasta_f,
 																	in_fasta_r=arguments.in_fasta_r,
 																	out_prefix=arguments.out_prefix,
-																	out_loc=output_dir_loc[2],
+																	out_loc=output_dir_loc[1],
 																	adaptor_path=arguments.adaptor_path,
 																	seed_mis=arguments.seed_mis,
 																	pal_clip=arguments.pal_clip,
@@ -149,7 +196,14 @@ if __name__ == '__main__':
 																	leadq=arguments.lead_q,
 																	trailq=arguments.trail_q,
 																	min_len=arguments.min_len)
-	# TODO Add BWA running
-	# run_bwa(threadNum=arguments.threadNum, trimmed_for_inputs=out_pair1,
-	# 		trimmed_rev_inputs=out_pair2, ref_gen=arguments.ref_g, out_prefix=arguments.out_prefix)
+
+	run_bwa_index(ref_gen=arguments.ref_g)
+
+	run_bwa_mem(thread_num=arguments.thread_n[0],
+													trimmed_for_inputs=out_pair1,
+													trimmed_rev_inputs=out_pair2,
+													out_prefix=arguments.out_prefix,
+													out_loc=output_dir_loc[2],
+													ref_gen=arguments.ref_g)
+
 	# TODO Add TIGER running
